88fd5c37bd894394cb76bd47e8cf5d00c566fdfa
esra akbulat <esra@klinikya.com>
2023-12-11 14:50:26 +0300

    url change

--- src/utils/functionsApi.js
+++ src/utils/functionsApi.js
@@ -2,7 +2,7 @@ import { RadioBrowserApi, StationSearchType } from 'radio-browser-api';
 
 export const getCountriesFunction = async (setFunction) => {
   const api = new RadioBrowserApi('My Radio App');
-  api.baseUrl = 'https://all.api.radio-browser.info';
+  api.baseUrl = 'http://all.api.radio-browser.info';
   const response = await api.getCountries();
   const temp = response?.filter((item) => item.stationcount > 10);
   temp?.map((item) =>
@@ -12,7 +12,7 @@ export const getCountriesFunction = async (setFunction) => {
 
 export const getTagsFunction = async (setFunction) => {
   const api = new RadioBrowserApi('My Radio App');
-  api.baseUrl = 'https://all.api.radio-browser.info';
+  api.baseUrl = 'http://all.api.radio-browser.info';
   const response = await api.getTags();
   const temp = response?.filter((item) => item?.stationcount > 100);
   temp?.map((item) =>
@@ -22,7 +22,7 @@ export const getTagsFunction = async (setFunction) => {
 
 export const getLanguagesFunction = async (setFunction) => {
   const api = new RadioBrowserApi('My Radio App');
-  api.baseUrl = 'https://all.api.radio-browser.info';
+  api.baseUrl = 'http://all.api.radio-browser.info';
   const response = await api.getLanguages();
   const temp = response?.filter((item) => item?.stationcount > 100);
   temp?.map((item) =>
